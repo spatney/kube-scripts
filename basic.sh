578a18aa4b2b5148f0640e7c88d75d11da5015b1
Sachin Patney <spatney@users.noreply.github.com>
2018-11-04 15:49:28 -0800

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -7,3 +7,5 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" \
     > /etc/apt/sources.list.d/kubernetes.list
 apt-get update && apt-get install -y kubeadm kubelet kubectl
+
+sudo sed -e '/swap/ s/^#*/#/' -i /etc/fstab
