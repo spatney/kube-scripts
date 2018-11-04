84cffb28c14dc159555f02d16c1b05d45f12e2c0
Sachin Patney <spatney@users.noreply.github.com>
2018-11-04 12:45:44 -0800

    Create basic-master.sh

---
+++ basic-master.sh
@@ -0,0 +1,11 @@
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update && apt install -qy docker-ce
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" \
+    > /etc/apt/sources.list.d/kubernetes.list
+apt-get update && apt-get install -y kubeadm kubelet kubectl
+
+kubeadm init --apiserver-advertise-address=$1 --pod-network-cidr=192.168.1.0/16
